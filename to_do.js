8ee0622c1fedc6a004a6a3515e954717929103b2
Alexandra Craig <alexandra.craig@gmail.com>
2016-09-29 20:32:22 -0400

    to do list table is working

--- to_do.js
+++ to_do.js
@@ -1,11 +1,12 @@
-var task = $('[name="task"]')
+var task = $('[name="task"]') // variable for textarea
 
-var addButton = $('#add_btn')
+var addButton = $('#add_btn') //variable for the button
 
-var tasks = $('#tasks')
+var tasks = $('#tasks') //variable for place info from texarea is going
 
-addButton.click(function(){
-  var text = task.val()
+addButton.click(function(){  //when button is clicked the function occurs
+  var text = task.val()      //the value of the textarea (task) is set to variable text
 
-  $('<li>').text(text).appendTo(tasks)
-})
+  $('<tr>').text(text).appendTo(tasks) //value from textarea, now called "text" due to function, is being appended to the unordered
+})                                      //as a <li> .  finding the .text(text) a little confusing..
+                                        //unless its like the <li>.text = text except with ()....4
